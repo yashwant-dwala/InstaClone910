a1f8fb46d3d2c49f2968dcfb3f8d5b52a4195e18
Yashwant Dwala <yashwant.19je0938@ece.iitism.ac.in>
2022-01-14 13:34:05 +0530

    make it better again

--- server/client/src/config.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios"
-
-export const axiosInstance = axios.create({
-    basseURL :"https://instaclone910.herokuapp.com/server"
-})
--- server/config/prod.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGOURI:
-    process.env.MONGOURI,
-  JWT_Secret: process.env.JWT_Secret,
-};
